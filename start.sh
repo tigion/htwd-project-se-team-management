446da1880649d19b61fa5ffe61baaf62d5e89410
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2023-09-15 16:23:40 +0200

    Refactor script

--- start.sh
+++ start.sh
@@ -2,15 +2,20 @@
 #
 # Start docker environment
 #
-# Options:
-#   --build|-b  ... start with new build images
-#   --update|-u ... start with updated base images
-#   --reset|-r  ... fresh start with removed containers, images and networks
+# Options (first only):
+#   --build     ... start with new build images
+#   --update    ... start with updated base images
+#   --reset     ... fresh start with removed containers, images and networks
 #   --reset-all ... fresh start with removed containers, images, networks and volumes
+# Options (second only):
+#   --no-start  ... run script with no Docker start
 
 # Clean up notes:
 # sudo docker system prune --all
 # sudo docker system prune --all --volumes
+
+# cd & check
+if ! cd "$(dirname "$0")"; then exit; fi
 
 # Need root for Linux
 if [[ "$(uname -s)" == "Linux" && "$(id -u)" -ne 0 ]]; then
@@ -18,33 +23,40 @@ if [[ "$(uname -s)" == "Linux" && "$(id -u)" -ne 0 ]]; then
   exit 1
 fi
 
-# cd & check
-if ! cd "$(dirname "$0")"; then exit; fi
-
-if [[ "$1" == "--build" || "$1" == "-b" ]]; then
-  # build images
-  docker compose build
-elif [[ "$1" == "--update" || "$1" == "-u" ]]; then
-  # pull newer images and build images
-  docker compose build --pull
-elif [[ "$1" == "--reset" || "$1" == "-r" ]]; then
+# Remove containers, images, networks and
+# with argument 'all' also volumes
+reset() {
   echo "This removes containers, images and networks!"
+  [[ $1 == "all" ]] && echo "This also removes volumes. Important data can be lost!"
+
   read -p "Are you sure you want to continue? [y/N] " -r -n 1 answer
   echo ""
-  if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
-    # remove containers, images and networks
+  [[ "$answer" != "y" && "$answer" != "Y" ]] && exit 0
+
+  if [[ $1 == "all" ]]; then
+    docker compose down --rmi all --volumes --remove-orphans
+  else
     docker compose down --rmi all --remove-orphans
   fi
+}
+
+# Parse script options
+if [[ "$1" == "--build" ]]; then
+  # Build images
+  docker compose build
+elif [[ "$1" == "--update" ]]; then
+  # Pull newer images and build images
+  docker compose build --pull
+elif [[ "$1" == "--reset" ]]; then
+  # Remove containers, images and networks
+  reset
 elif [[ "$1" == "--reset-all" ]]; then
-  echo "This removes containers, images and networks!"
-  echo "This also removes volumes. Important data can be lost!"
-  read -p "Are you sure you want to continue? [y/N] " -r -n 1 answer
-  echo ""
-  if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
-    # remove containers, images, networks and volumes
-    docker compose down --rmi all --volumes --remove-orphans
-  fi
+  # Remove containers, images, networks and volumes
+  reset all
 fi
 
-# start docker environment
+# Exit without start
+[[ "$2" == "--no-start" ]] && exit 0
+
+# Start docker environment
 docker compose up -d
