02f032625221a1b88f4f949712aaadc99d95a89c
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2025-08-29 12:06:03 +0200

    refactor: correct and clean up some things

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
